395c70805f40c007cb190260c8a60a9740e555a7
Kostya Hospodarysko <khospodarysko@lohika.com>
2018-01-11 11:43:54 +0200

    Extra test.

---
+++ src/test/java/com/khospodarysko/ButtonTest.java
@@ -0,0 +1,13 @@
+package com.khospodarysko;
+
+import org.testng.annotations.Test;
+
+/**
+ * Click on button 5 times and assert that button's text is "Clicked 5"
+ */
+public class ButtonTest extends BaseTest {
+    @Test
+    public void testExpectedCondition() {
+        driver.get("file:///Users/khospodarysko/projects/training-selenium/src/main/resources/button.html");
+    }
+}
