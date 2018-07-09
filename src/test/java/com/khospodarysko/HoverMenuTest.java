17d8890363308f2693c4aafd8cbccd872b04c8db
Kostyantyn Hospodarysko <khospodarysko@lohika.com>
2018-07-09 13:27:17 +0300

    changes

--- src/test/java/com/khospodarysko/HoverMenuTest.java
+++ src/test/java/com/khospodarysko/HoverMenuTest.java
@@ -1,6 +1,7 @@
 package com.khospodarysko;
 
 import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -14,7 +15,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 public class HoverMenuTest extends BaseTest {
-    @Test(invocationCount = 10)
+    @Test(invocationCount = 5)
     public void testExpectedCondition() throws InterruptedException {
         driver.get("file://" + absoluteFilePath("hover-menu"));
         TimeUnit.SECONDS.sleep(2);
@@ -24,14 +25,18 @@ public class HoverMenuTest extends BaseTest {
 
         LocalTime now = LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
 
-        Actions hover = new Actions(driver);
-        hover.moveToElement(page.hover1).moveToElement(page.hover2).click(page.hover3).build().perform();
+        // Actions hover = new Actions(driver);
+        // hover.moveToElement(page.hover1).moveToElement(page.hover2).click(page.hover3).build().perform();
+
+        page.openMenuItem("Hover me for more options", "Even More..", "final level");
 
         // Assertions.assertThat(page.getTime()).isEqualTo(now);
-        Assertions.assertThat(page.getTime()).isEqualToIgnoringNanos(now);
+        Assertions.assertThat(page.getTime()).isEqualToIgnoringNanos(now.plusSeconds(2));
     }
 
     private static class Page {
+        WebDriver driver;
+
         @FindBy(id = "dLabel")
         public WebElement button;
 
@@ -41,18 +46,40 @@ public class HoverMenuTest extends BaseTest {
         @FindBy(xpath = "//a[text()='Even More..']")
         public WebElement hover2;
 
-        @FindBy(xpath = "//a[text()='2nd 3rd level']")
+        @FindBy(xpath = "//a[text()='final level']")
         public WebElement hover3;
 
         @FindBy(id = "text")
         private WebElement text;
 
         public Page(WebDriver driver) {
+            this.driver = driver;
             PageFactory.initElements(driver, this);
         }
 
         public LocalTime getTime() {
             return LocalTime.parse(text.getText(), DateTimeFormatter.ofPattern("h:mm:ss a"));
+        }
+
+        public void openMenuItem(String... items) throws InterruptedException {
+            Actions action = new Actions(driver);
+            WebElement first = null;
+            for (int i = 0; i < items.length - 1; i++) {
+                if (i == 0) {
+                    first = driver.findElement(By.xpath("//*[text()='" + items[0] + "']"));
+                    action.moveToElement(first).perform();
+                    TimeUnit.SECONDS.sleep(1);
+                } else {
+                    first = first.findElement(By.xpath(".././/*[text()='" + items[i] + "']"));
+                    action.moveToElement(first).perform();
+                    TimeUnit.SECONDS.sleep(1);
+                }
+
+            }
+
+            WebElement element = first.findElement(By.xpath(".././/*[text()='" + items[items.length - 1] + "']"));
+            action.click(element).perform();
+            TimeUnit.SECONDS.sleep(1);
         }
     }
 }
