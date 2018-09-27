24550387280737a3b19874f541ca6024ab55e9b3
Kostyantyn Hospodarysko <khospodarysko@lohika.com>
2018-09-27 10:40:25 +0300

    button wait ways

--- src/test/java/com/khospodarysko/ButtonTest.java
+++ src/test/java/com/khospodarysko/ButtonTest.java
@@ -2,9 +2,15 @@ package com.khospodarysko;
 
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nullable;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 /**
@@ -19,11 +25,40 @@ public class ButtonTest extends BaseTest {
     public void testExpectedCondition() {
         driver.get("file://" + absoluteFilePath("1. button"));
 
+        // built-in
+        new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.tagName("button")));
+        // lambda
         new WebDriverWait(driver, 20).until(driver -> driver.findElement(By.tagName("button")).isDisplayed());
+        // separate class
+        new WebDriverWait(driver, 20).until(new VisibleByText("text"));
+        // predicate
+        new WebDriverWait(driver, 20).until(d -> dd("kostya"));
 
         IntStream.rangeClosed(1, 5).forEach(i -> driver.findElement(By.tagName("button")).click());
 
         Assertions.assertThat(driver.findElement(By.tagName("button")).getText())
             .isEqualTo("Clicked 5");
+    }
+
+    private Predicate<WebDriver> dd(String text) {
+        return driver -> {
+            WebElement element = driver.findElement(By.xpath("//*[text()='" + text + "']"));
+            return element.isDisplayed();
+        };
+    }
+}
+
+class VisibleByText implements ExpectedCondition<WebElement> {
+    private final String text;
+
+    public VisibleByText(String text) {
+        this.text = text;
+    }
+
+    @Nullable
+    @Override
+    public WebElement apply(@Nullable WebDriver driver) {
+        WebElement element = driver.findElement(By.xpath("//*[text()='" + text + "']"));
+        return element.isDisplayed() ? element : null;
     }
 }
